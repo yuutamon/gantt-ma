0e3f9e63ac263df5ecf1c06978bd6ccc92cc4053
ゆうたもん <121707206+yuutamon@users.noreply.github.com>
2026-06-18 12:26:58 +0900

    Add files via upload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,47 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+import LoginForm from './components/LoginForm.vue'
+import GanttView from './components/GanttView.vue'
 
+const auth = useAuthStore()
+onMounted(auth.init)
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="comfortable" flat>
+      <v-app-bar-title>Vue3 + DRF ガントチャート</v-app-bar-title>
+      <template #append v-if="auth.user">
+        <span class="text-body-2 mr-3">{{ auth.user.username }}</span>
+        <v-btn variant="text" prepend-icon="mdi-logout" @click="auth.logout">ログアウト</v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <div v-if="auth.initializing" class="loading-center">
+        <v-progress-circular indeterminate color="primary" />
+      </div>
+      <LoginForm v-else-if="!auth.user" />
+      <div v-else class="main-area">
+        <GanttView />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+.main-area {
+  height: calc(100vh - 64px);
+}
+.loading-center {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+  .use(createPinia())
+  .use(vuetify)
+  .mount('#app')
